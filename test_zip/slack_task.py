5af42be1480f7031cbe01812eea1522663c8b951
arbalan33 <arbalan@griddynamics.com>
2025-05-30 17:37:08 +0300

    add vault

--- test_zip/slack_task.py
+++ test_zip/slack_task.py
@@ -1,20 +1,14 @@
 from datetime import datetime
 
-from airflow import DAG
-from airflow.operators.empty import EmptyOperator
-from airflow.operators.python import PythonOperator
-from airflow.decorators import dag, task
-from airflow.operators.bash import BashOperator
-from airflow.providers.http.hooks.http import HttpHook
+from airflow.decorators import task
+from airflow.models import Variable
 
 
 from slack_utils import slack_utils
 
 @task
 def slack_send_task(**context):
-    hook = HttpHook(http_conn_id='slack_conn')
-    extra = hook.get_connection(hook.http_conn_id).extra_dejson
-    slack_token = extra['token']
+    slack_token = Variable.get("slack_token")
 
     return slack_utils.slack_send(slack_token,
                                   "social",
